7d3cbc369544e9fcab4091e607023633c26d9f72
juniper wilde <juniper.r.wilde@gmail.com>
2019-03-02 23:36:22 -0800

    fix capture.sh, windows vscode shell settings

--- utility/capture.sh
+++ utility/capture.sh
@@ -1,11 +1,11 @@
 #!/bin/bash
 
 echo "rounding up my dot file children"
-cp ~/.bash_profile ../configs/.bash_profile
-cp ~/.gitconfig ../configs/.gitconfig
+cp ~/.bash_profile ./configs/.bash_profile
+cp ~/.gitconfig ./configs/.gitconfig
 
 case "$(uname -s)" in
     Linux*)     echo "WHERE DO I GET THESE OH NO";;
-    Darwin*)    cp ~/Library/Application\ Support/Code/User/settings.json ../configs/settings.json;;
-    MINGW*)     cp ~/AppData/Roaming/Code/User/settings.json ../configs/settings.json;;
+    Darwin*)    cp ~/Library/Application\ Support/Code/User/settings.json ./configs/settings.json;;
+    MINGW*)     cp ~/AppData/Roaming/Code/User/settings.json ./configs/settings.json;;
 esac
