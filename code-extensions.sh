bda9b6bd57a681ae0b2a8e7c21e38160fc3b6349
juniper wilde <juniper.r.wilde@gmail.com>
2020-08-05 11:19:25 -0700

    clean up vscode extension install step

---
+++ code-extensions.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+
+code --install-extension 13xforever.language-x86-64-assembly
+code --install-extension bungcip.better-toml
+code --install-extension dbaeumer.vscode-eslint
+code --install-extension dzannotti.vscode-babel-coloring
+code --install-extension ecmel.vscode-html-css
+code --install-extension equinusocio.vsc-material-theme
+code --install-extension equinusocio.vsc-community-material-theme
+code --install-extension equinusocio.vsc-material-theme-icons
+code --install-extension esbenp.prettier-vscode
+code --install-extension firefox-devtools.vscode-firefox-debug
+code --install-extension github.vscode-codeql
+code --install-extension hbenl.vscode-test-explorer
+code --install-extension mrcrowl.hg
+code --install-extension ms-python.python
+code --install-extension ms-vscode.cpptools
+code --install-extension mythmon.idl
+code --install-extension nathanridley.autotrim
+code --install-extension naumovs.color-highlight
+code --install-extension wayou.vscode-todo-highlight
+code --install-extension xaver.clang-format
--- setup-windows.sh
+++ setup-windows.sh
@@ -6,27 +6,7 @@ echo 'installing windows setup'
 curl https://sh.rustup.rs -sSf | sh
 
 # code extensions
-code --install-extension nathanridley.autotrim
-code --install-extension metalcanine.awoo
-code --install-extension dzannotti.vscode-babel-coloring
-code --install-extension bungcip.better-toml
-code --install-extension ms-vscode.cpptools
-code --install-extension xaver.clang-format
-code --install-extension twxs.cmake
-code --install-extension github.codeql
-code --install-extension naumovs.color-highlight
-code --install-extension firefox-devtools.vscode-firefox-debug
-code --install-extension dbaeumer.eslint
-code --install-extension mrcrowl.hg
-code --install-extension ecmel.vscode-html-css
-code --install-extension mythmon.idl
-code --install-extension esbenp.prettier-vscode
-code --install-extension ms-python.python
-code --install-extension rust-lang.rust
-code --install-extension wayou.vscode-todo-highlight
-code --install-extension equinusocio.vsc-material-theme
-code --install-extension equinusocio.vsc-material-theme-icons
-code --install-extension 13xforever.language-x86-64-assembly
+source code-extensions.sh
 
 # BE FREE MY PRETTIES
 ./utility/release.sh
